18903f94864ea3696ec4059341e22ba4bce44cea
Philippe Recto <precto1285@gmal.com>
2019-03-01 09:43:44 -0800

    changed BandThree to class component. added constructor

--- change-band/src/BandThree.js
+++ change-band/src/BandThree.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import Title from './SubCompThree.js/Title';
 
 class BandThree extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'Galeng'
+    }
+  }
   render() {
     return (
       <div>
-        <Title title="Galeng!" />
+        <Title title={this.state.title} />
         <p>Instructions: Add your super awesome band name!</p>
         <input />
       </div>
